c369097083dc08a2d0976006a75d6b686ac15b93
thabilemngoma <mkehmngoma@gmail.com>
2020-01-14 10:58:05 +0200

    passwordchecker

---
+++ src/test/java/PasswordValidationTest.java
@@ -0,0 +1,46 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidationTest {
+    PasswordValidation validations = new PasswordValidation();
+
+    @Test
+    void existence() {
+        String password = "lyert";
+
+        assertTrue(validations.existence(password));
+    }
+
+    @Test
+    void longer() {
+        String password = "hdhkltgq";
+        assertTrue(validations.longer(password));
+    }
+
+    @Test
+    void lowerCase() {
+        String password = "KHHHHHk";
+        assertTrue(validations.lowerCase(password));
+    }
+
+    @Test
+    void upperCase() {
+        String password = "Ahytrefdsh";
+        assertTrue(validations.upperCase(password));
+    }
+
+    @Test
+    void digit() {
+        String password = "hh^rr56";
+        assertTrue(validations.digit(password));
+    }
+
+    @Test
+    void character() {
+        String password = "ljjf@kjhkkj";
+        assertTrue(validations.character(password));
+    }
+}
+
